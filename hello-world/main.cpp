4be4bfd627500d30dc4426b28921758c1e8edef1
Prasidh Aggarwal <aprasidh@gmail.com>
2022-03-07 13:34:28 +0530

    Initial Commit

---
+++ hello-world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  hello-world
+//
+//  Created by Prasidh Aggarwal on 07/03/22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
